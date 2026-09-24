5ae211626cd97e7f294024d661e95afa5263497c
bingert <sven.bingert@gwdg.de>
2026-09-24 13:45:33 +0200

    another option to visualize

--- create_mermaid_diagram.py
+++ create_mermaid_diagram.py
@@ -32,23 +32,32 @@ with open('type-system-overview.md','w') as f:
                 f.write( val + '`\n')
         f.write('}\n')
     # find relations
-    ts2=ts
-    for elem1 in ts:
-        ent1 = elem1.replace('0.FDO/','O.FDO-')
-        for elem2 in ts2:
-            if elem1 != elem2:
-#                if elem1 == '0.FDO/Profile':
-#                    print('---------------------' + elem2 )
-                found = False
-                for att in ts2[elem2]:
- #                   if elem2 == '0.FDO/Cardinality':
- #                       print(elem1 +'-----------' +list(att.values())[0]+'--')
-                    if list(att.values())[0].strip() == elem1 and not found:
-                        ent2 = elem2.replace('0.FDO/','O.FDO-')
-                        f.write(ent1 + ' ||--|| '+ ent2 + ' : ref \n')
-                        found = True
-                        
-                    
+    option = 2
+    if option == 1:
+        for elem1 in ts:
+            ent1 = elem1.replace('0.FDO/','O.FDO-')
+            for elem2 in ts:
+                if elem1 != elem2:
+                    found = False
+                    for att in ts[elem2]:
+                        if list(att.keys())[0].strip() == elem1 and not found:
+                            ent2 = elem2.replace('0.FDO/','O.FDO-')
+                            f.write(ent1 + ' ||--|| '+ ent2 + ' : ref \n')
+                            found = True
+    elif option == 2:
+        links = []
+        for elem1 in ts:
+            ent1 = elem1.replace('0.FDO/','O.FDO-')
+            for att in ts[elem1]:
+                ent2 = list(att.keys())[0].strip().replace('0.FDO/','O.FDO-')
+                link = ent1 + ' ||--|| '+ ent2 + ' : requires \n'
+                links.append(link)
+        # remove duplicates
+        links = list(set(links))
+        for link in links:
+            f.write(link)
+
+
 
     #CUSTOMER ||--o{ ORDER : places
     #ORDER ||--|{ ORDER_ITEM : contains
